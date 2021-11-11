9017f7c6a9eab654d6edf4fed57c3a4d9374603f
Ricardo Calado <jricardoc10@gmail.com>
2021-11-11 00:30:07 -0300

    criando configuracao para o pipeline

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -16,7 +16,7 @@ config :my_phx_app, MyPhxAppWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # ## Using releases (Elixir v1.9+)
 #
